0cf39320f305d9b15302d0177bfd4b352f0a003b
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 20:26:14 -0600

    remove punctuation to movie title being compared to query

--- cli/search.py
+++ cli/search.py
@@ -1,13 +1,15 @@
 import json
 from search_functions import load_movies
+import string
 
 def search_command(query: str) -> list[str]:
 
     data = load_movies()
 
     movies_in_query = []
+    remove_table = str.maketrans("", "", string.punctuation)
     for movie in data['movies']:
-        if query in movie['title'].lower():
+        if query in movie['title'].lower().translate(remove_table):
             movies_in_query.append(movie['title'])
 
     return movies_in_query
