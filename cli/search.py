4b8b9f6dad6399dae8644ebd0e218690e4e400d7
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 20:34:29 -0600

    move the preprocessing to the search_function.py file

--- cli/search.py
+++ cli/search.py
@@ -1,15 +1,14 @@
 import json
-from search_functions import load_movies
-import string
+from search_functions import load_movies, preprocess_text
+
 
 def search_command(query: str) -> list[str]:
 
     data = load_movies()
 
     movies_in_query = []
-    remove_table = str.maketrans("", "", string.punctuation)
     for movie in data['movies']:
-        if query in movie['title'].lower().translate(remove_table):
+        if preprocess_text(query) in preprocess_text(movie['title']):
             movies_in_query.append(movie['title'])
 
     return movies_in_query
