0928a32ffb5437eafe14d9b38898986b0224f4cb
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 21:32:33 -0600

    add function to compare tokens and return a true if so and false otherwise, also add a function to filter out tokens that are contained in the stopwords list

--- cli/search.py
+++ cli/search.py
@@ -1,5 +1,20 @@
 import json
-from search_functions import load_movies, tokenize
+from search_functions import load_movies, tokenize, load_stopwords
+
+def filter_tokens(tokens: list[str]) -> list[str]:
+    stopwords: list[str] = load_stopwords()
+    filtered_tokens = []
+    for token in tokens:
+        if token not in stopwords:
+            filtered_tokens.append(token)
+    return filtered_tokens
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
 
 
 def search_command(query: str) -> list[str]:
@@ -8,11 +23,9 @@ def search_command(query: str) -> list[str]:
 
     movies_in_query = []
     for movie in data['movies']:
-        query_tokens: list[str] = tokenize(query)
-        title_tokens: list[str] = tokenize(movie['title'])
-        for query_token in query_tokens:
-            for title_token in title_tokens:
-                if query_token in title_token and movie['title'] not in movies_in_query:
-                    movies_in_query.append(movie['title'])
+        query_tokens: list[str] = filter_tokens(tokenize(query))
+        title_tokens: list[str] = filter_tokens(tokenize(movie['title']))
+        if has_matching_token(query_tokens, title_tokens) and movie['title'] not in movies_in_query:
+            movies_in_query.append(movie['title'])
 
     return movies_in_query
