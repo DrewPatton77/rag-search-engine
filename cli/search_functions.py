135d8b039326405ed42af180832efb51daba51c3
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 21:31:24 -0600

    add function to load a .txt file of stopwords and returns the stopwords as a list of strings preprocessed

--- cli/search_functions.py
+++ cli/search_functions.py
@@ -8,14 +8,24 @@ class Movie(TypedDict):
     descirption: str
 
 DATA_PATH = "Data/movies.json"
+STOPWORDS_PATH = "Data/stopwords.txt"
+
 def load_movies() -> list[Movie]:
     with open(DATA_PATH, "r") as f:
         data = f.read()
-
     return json.loads(data)
+
 
 def preprocess_text(text: str) -> str:
     return text.lower().translate(str.maketrans("","",string.punctuation))
 
 def tokenize(text: str) -> list[str]:
     return preprocess_text(text).split()
+
+def load_stopwords() -> list[str]:
+    with open(STOPWORDS_PATH, "r") as f:
+        stopwords_preprocessed= f.read().splitlines()
+    stopwords = []
+    for stopword in stopwords_preprocessed:
+        stopwords.append(preprocess_text(stopword))
+    return stopwords
