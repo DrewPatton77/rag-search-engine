fb81cacd85f0a3728ef734983c664d59a7d669fb
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 20:03:56 -0600

    add a match function that checks the query to all the movies and appends matches sorted in ascending movie id by default

---
+++ cli/search.py
@@ -0,0 +1,13 @@
+import json
+from search_functions import load_movies
+
+def search_command(query: str) -> list[str]:
+
+    data = load_movies()
+
+    movies_in_query = []
+    for movie in data['movies']:
+        if query in movie['title']:
+            movies_in_query.append(movie['title'])
+
+    return movies_in_query
