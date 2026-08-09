4f9baca496fd18c12d770c8308466df00051397f
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 21:05:05 -0600

    add matching to at least one query token appears in any title token

--- cli/search.py
+++ cli/search.py
@@ -1,5 +1,5 @@
 import json
-from search_functions import load_movies, preprocess_text
+from search_functions import load_movies, tokenize
 
 
 def search_command(query: str) -> list[str]:
@@ -8,7 +8,11 @@ def search_command(query: str) -> list[str]:
 
     movies_in_query = []
     for movie in data['movies']:
-        if preprocess_text(query) in preprocess_text(movie['title']):
-            movies_in_query.append(movie['title'])
+        query_tokens: list[str] = tokenize(query)
+        title_tokens: list[str] = tokenize(movie['title'])
+        for query_token in query_tokens:
+            for title_token in title_tokens:
+                if query_token in title_token and movie['title'] not in movies_in_query:
+                    movies_in_query.append(movie['title'])
 
     return movies_in_query
