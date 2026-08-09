904ebeb6695c3c386d78dcab84650131bec41f2d
LilyFae <andrew.patton@ucalgary.ca>
2026-08-08 20:02:54 -0600

    add a movie class and a load_movies() function to load the movies.json data as a python dictionary

---
+++ cli/search_functions.py
@@ -0,0 +1,16 @@
+import json
+from search_constants import *
+
+from typing import Any, TypedDict
+
+class Movie(TypedDict):
+    id: int
+    title: str
+    descirption: str
+
+DATA_PATH = "Data/movies.json"
+def load_movies() -> list[Movie]:
+    with open(DATA_PATH, "r") as f:
+        data = f.read()
+
+    return json.loads(data)
