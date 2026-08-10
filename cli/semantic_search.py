b9397b7b3fe31d79b25d5c3a9151f480f2aa7a48
LilyFae <andrew.patton@ucalgary.ca>
2026-08-10 08:51:23 -0600

    add embed_text() functionthat embeds a given text to embedding space as a vector

--- cli/semantic_search.py
+++ cli/semantic_search.py
@@ -4,6 +4,20 @@ class SemanticSearch:
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
 
+    def generate_embedding(self, text: str):
+        if text.strip() == "":
+            raise ValueError("The text is empty")
+
+        embedding = self.model.encode([text])
+        return embedding[0]
+
+def embed_text(text: str):
+    sems = SemanticSearch()
+    embedding = sems.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
 def verify_model():
     sems = SemanticSearch()
     print(f"Model loaded: {sems.model}")
