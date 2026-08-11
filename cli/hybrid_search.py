795a219be3c2b132425a508ad313b8931014497a
LilyFae <andrew.patton@ucalgary.ca>
2026-08-11 16:06:34 -0600

    add a reciprocal rank fusion function

--- cli/hybrid_search.py
+++ cli/hybrid_search.py
@@ -20,27 +20,34 @@ class HybridSearch:
         self.idx.save()
         self.idx.bm25_search(query, limit = limit)
 
-    def weighted_search(self, query: str, alpha: float, limit: int = 5) -> list[dict]:
-        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
-
-    def rrf_search(self, query: str, k: int, limit: int = 10) -> list[dict]:
-        raise NotImplementedError("RRF hybrid search is not implemented yet.")
 
     def weighted_search(self, query: str, alpha: float = 0.5, limit: int = 5):
+        method = "min_max"
+        self.hybrid_scores_mapping(method, query, alpha=alpha, limit=limit)
+
+    def rrf_search(self, query: str, k: int = 60, limit: int = 10) -> list[dict]:
+        method = "rrf"
+        self.hybrid_scores_mapping(method, query, k=k, limit=limit)
+
+    def hybrid_scores_mapping(self, method, query: str, alpha: float = 0.5, k: int = 60, limit: int = 10):
+        if method == "None":
+            raise Exception("method is None. Input method = 'min_max' for min-max normalization or 'rrf' for reciprocal rank fusion.")
+
         self._bm25_search(query, 500*limit)
         keyword_scores = [self.idx.score[key] for key in self.idx.score]
-        keyword_scores_normalized = min_max_norm_score(keyword_scores)
+        if method == "min_max":
+            keyword_scores = [self.idx.score[key] for key in self.idx.score]
+            keyword_scores_normalized = min_max_norm_score(keyword_scores)
+        if method == "rrf":
+            keyword_scores_normalized = [i for i in range(0, len(self.idx.score))]
+            #keyword_scores_normalized = rrf_score(keyword_scores)
+
         for i, key in enumerate(self.idx.score):
             self.idx.score[key] = keyword_scores_normalized[i]
 
         semantic_score_list = self.semantic_search.search_chunks(query, limit=500*limit)
-        semantic_scores_normalized = min_max_norm_score_semantics(semantic_score_list)
+        semantic_scores_normalized = norm_score(semantic_score_list, method=method)
 
-        #print(len(semantic_scores_normalized))
-        #print(len(self.idx.score))
-        #semantic -> list of dictionaries, id: id, score: score, etc...
-        #keyword -> dictionary, id: score
-        # Make a list of dictionaries like semantic
         hybrid_scores_docmap = {}
         for id in self.idx.score:
             hybrid_scores_docmap[id] = {
@@ -60,20 +67,28 @@ class HybridSearch:
                 }
             hybrid_scores_docmap[id]['semantic_score'] = score
 
+
         for id in hybrid_scores_docmap:
             keyword_score = hybrid_scores_docmap[id]['keyword_score']
             semantic_score = hybrid_scores_docmap[id]['semantic_score']
-            hybrid_scores_docmap[id]['hybrid_score'] = hybrid_score(keyword_score, semantic_score, alpha=alpha)
+            if method == "min_max":
+                hybrid_scores_docmap[id]['hybrid_score'] = hybrid_score(keyword_score, semantic_score, alpha=alpha, method=method)
+            if method == "rrf":
+                hybrid_scores_docmap[id]['hybrid_score'] = rrf_score(keyword_score) + rrf_score(semantic_score)
 
         sorted_data = dict(sorted(hybrid_scores_docmap.items(), key=lambda item: item[1]['hybrid_score'], reverse=True))
         for i, id in enumerate(sorted_data):
             if i < limit:
-                print(f"{i + 1}. {sorted_data[id]['document']['title']} (score: {sorted_data[id]['hybrid_score']})")
-
-
-
-
-
+                if method == "rrf":
+                    print(f"{i + 1}. {sorted_data[id]['document']['title']}")
+                    print(f"RRF Score: {sorted_data[id]['hybrid_score']:.3f}")
+                    print(f"BM25 Rank: {sorted_data[id]['keyword_score']}, Semantic Rank: {sorted_data[id]['semantic_score']}")
+                    print(f"{sorted_data[id]['document']['description'][:100]}...")
+                if method == "min_max":
+                    print(f"{i + 1}. {sorted_data[id]['document']['title']}")
+                    print(f"Hybrid Score: {sorted_data[id]['hybrid_score']:.3f}")
+                    print(f"BM25: {sorted_data[id]['keyword_score']}, Semantic: {sorted_data[id]['semantic_score']}")
+                    print(f"{sorted_data[id]['document']['description'][:100]}...")
 
 def min_max_norm_score(scores: list[int]) -> None:
     if len(scores) == 0:
@@ -91,17 +106,37 @@ def min_max_norm_score(scores: list[int]) -> None:
     #for score in normalized_scores:
         #print(f"* {score:.4f}")
 
-def min_max_norm_score_semantics(score_dict_list: list[dict]) -> list[dict]:
+def norm_score(score_dict_list: list[dict], method: str | None = None) -> list[dict]:
     scores = [d.get("score") for d in score_dict_list]
-    scores_normalized = min_max_norm_score(scores)
+
+    if method == None:
+        raise Exception("method is None. Input method = 'min_max' for min-max normalization or 'rrf' for reciprocal rank fusion.")
+
+    if method == "min_max":
+        scores_normalized = min_max_norm_score(scores)
+
+    if method == "rrf":
+        scores_normalized = [i for i in range(0, len(scores))] # Ranked.
+        #scores_normalized = rrf_score(scores)
+
     for i in range(0, len(scores_normalized)):
         score_dict_list[i]["score"] = scores_normalized[i]
     return score_dict_list
 
-def hybrid_score(bm25_score: float, semantic_score: float, alpha: float = 0.5) -> float:
-    return alpha * bm25_score + (1 - alpha) * semantic_score
+def hybrid_score(bm25_score: float, semantic_score: float, alpha: float = 0.5, k: int = 60, method: str | None = None) -> float:
+    if method == "min_max":
+        return alpha * bm25_score + (1 - alpha) * semantic_score
+    return bm25_score + semantic_score
 
 def weighted_search(query: str, alpha: float = 0.5, limit: int = 5):
     documents = load_movies()
     hybrid = HybridSearch(documents['movies'])
     hybrid.weighted_search(query, alpha=alpha, limit=limit)
+
+def rrf_score(rank: int, k: int = 60):
+    return 1 / (k + rank)
+
+def rrf_search(query, k: int = 60, limit: int = 5) -> None:
+    documents = load_movies()
+    hybrid = HybridSearch(documents['movies'])
+    hybrid.rrf_search(query, k=k, limit=limit)
