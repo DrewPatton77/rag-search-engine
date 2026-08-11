464f8bc1ea58637632482a5573dbd44d9c23dfce
LilyFae <andrew.patton@ucalgary.ca>
2026-08-11 14:40:17 -0600

    add a hybrid search command

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -8,6 +8,15 @@ def main() -> None:
     normalize_parser = subparsers.add_parser("normalize", help="Normalize the cosine similarity and the BM25 scores using min-max normalization")
     normalize_parser.add_argument("scores", type=float, nargs="*", help="A list of scores to normalize using min-max normalization")
 
+    weighted_search_parser = subparsers.add_parser("weighted-search", help="Weighs the search return")
+    weighted_search_parser.add_argument("query", type=str, help="The query to search for")
+    weighted_search_parser.add_argument("-a", "--alpha", type=float, nargs="?", default=0.5, help="The weight parameter. A proportional ratio between the keyword:semantic. 1 - gives a 100% keyword return, 0 - gives a 100% semantic return, 0.5 - 50/50 split, etc...")
+    weighted_search_parser.add_argument("-l", "--limit", type=int, nargs="?", default=5, help="The number of results to return")
+
+    rrf_search_parser = subparsers.add_parser("rrf-search", help="Reciprocal Rank Fusion (rrf), instead of normalizing this method uses the rank of the score.")
+    rrf_search_parser.add_argument("query", type=str, help="The query to search for")
+    rrf_search_parser.add_argument("-k", type=int, nargs="?", default=60, help="A parameter that controls how much more weight we give to higher-ranked results vs lower-ranked ones. Where a lower k values like 20 gives more weight to top-ranked results, creating a steep drop-off in score. While higher k values like 100 creates a more gradual decline, giving lower-ranked results more influence")
+    rrf_search_parser.add_argument("-l", "--limit", type=int, nargs="?", default=5, help="The number of results to return")
 
     args = parser.parse_args()
 
@@ -16,6 +25,12 @@ def main() -> None:
         case "normalize":
             scores = args.scores
             min_max_norm_score(scores)
+
+        case "weighted-search":
+            query = args.query
+            alpha = args.alpha
+            limit = args.limit
+            weighted_search(query, alpha=alpha, limit=limit)
 
         case _:
             parser.print_help()
