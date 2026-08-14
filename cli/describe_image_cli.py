9e48f2d5423ed9cc7da4d7aed433d9bee878f61f
LilyFae <andrew.patton@ucalgary.ca>
2026-08-13 23:42:26 -0600

    add multimodal (text and image) input and return llm response

---
+++ cli/describe_image_cli.py
@@ -0,0 +1,27 @@
+import argparse
+import mimetypes
+from call_llm import llm_image
+import base64
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Multimodal CLI")
+    parser.add_argument("--image", type=str, nargs="?", help="The file path for the image to be embedded")
+    parser.add_argument("--query", type=str, nargs="?", help="The query to be embedded")
+
+    args = parser.parse_args()
+
+    image = args.image
+    query = args.query
+
+    mime, _ = mimetypes.guess_type(image)
+    mime = mime or "image/jpeg"
+
+    with open(image, "rb") as f:
+        img = f.read()
+
+    data_url = f"data:{mime};base64,{base64.b64encode(img).decode()}"
+    llm_image(data_url, query)
+
+
+if __name__ == "__main__":
+    main()
